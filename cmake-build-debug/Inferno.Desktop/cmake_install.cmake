7b723c007aa5e956cac8a7dee33cda7a4a28edbe
Rover656 <rover656@users.noreply.github.com>
2018-11-10 15:22:43 +0000

    Linux build support confirmed.

--- cmake-build-debug/Inferno.Desktop/cmake_install.cmake
+++ cmake-build-debug/Inferno.Desktop/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Reece Mackie/Documents/GitHub/InfernoPP/Inferno.Desktop
+# Install script for directory: /home/reece/CLionProjects/InfernoPP/Inferno.Desktop
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Inferno")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -25,6 +25,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   else()
     set(CMAKE_INSTALL_COMPONENT)
   endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
 endif()
 
 # Is this installation the result of a crosscompile?
